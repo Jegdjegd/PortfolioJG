2ee309a03257154d810e3684cde2e374998d3844
Jhoann G <jhoannegd@gmail.com>
2025-06-18 04:55:25 +0200

    feat:certifications

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -11,6 +11,12 @@ import {MatChipsModule} from '@angular/material/chips';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExperienceComponent {
+
+  igrowkerText = `A través de este proyecto, pude poner en práctica mis conocimientos existentes en desarrollo frontend con React y Material-UI para la construcción de interfaces de usuario, sino que también adquirí experiencia en la automatización de procesos. Específicamente, aprendí a implementar y gestionar flujos de trabajo de integración y despliegue continuo (CI/CD) utilizando GitHub Actions y Pipelines para entornos en GCP (Google Cloud Platform).`;
+
+  igrowkerProjects = `Tuve la oportunidad de colaborar en varios proyectos, algunos son los siguientes:`;
+
+  
   westerosText = `Es un mundo donde tienes al alcance toda la información sobre la famosa serie Game of Thrones, sus precuelas y secuelas; incluso encontrarás algún dragón volando por el sitio. El sistema realiza consultas a dos (2) APIs distintas para obtener toda la información. Desarrollado usando:`;
   
   btfxText = `La función del sitio es permitir que diferentes organismos de competición para ciclistas tengan acceso a un sistema fácil e intuitivo donde puedan inscribirse en pruebas ciclistas de todo tipo y, además, consultar las clasificaciones de todos los competidores registrados. Desarrollado usando:`;
